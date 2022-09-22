3e9458fe992f67858087e473f9d7b848f4fdb051
DarkGove <50306394+DarkGove@users.noreply.github.com>
2022-09-22 23:11:54 +0200

    test
    dockerfile not function

--- spring-docker/src/main/java/com/open/spring/gitlab/api/SpringGitlabApplication.java
+++ spring-docker/src/main/java/com/open/spring/gitlab/api/SpringGitlabApplication.java
@@ -2,10 +2,17 @@ package com.open.spring.gitlab.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringGitlabApplication {
+public class SpringGitlabApplication extends SpringBootServletInitializer{
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringGitlabApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitlabApplication.class, args);
 	}
